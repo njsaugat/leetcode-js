c3094fde54d2106e61d8b1c8e1e978cc6b3885dc
njsaugat <njsaugat@gmail.com>
2024-02-27 12:19:00 +0545

    leetcode a day

--- search_rotated.py
+++ search_rotated.py
@@ -1,35 +1,23 @@
 class Solution:
+    
     def search(self,nums,target):
-        
         left,right=0,len(nums)-1
         
-        while left<right:
+        while left<=right:
             mid=left+(right-left)//2
-            if nums[mid]==target:
+            
+            if target==nums[mid]:
                 return mid
-
-            elif nums[left]<nums[mid]:
-                # [7,0,1,2,4,5,6]
-                # 7>6 target>nums[mid] -->left=mid+1
-                # 4<6 target<nums[mid] -->right=mid-1
-                # 2<4 target<nums[left] -->left=mid+1
-                # 4>7 4>5 4<4
+            
+            elif nums[left]<=nums[mid]:
                 if target>nums[mid] or target<nums[left]:
                     left=mid+1
                 else:
                     right=mid-1
-                    
+            
             else:
-                if target<nums[mid] or target>nums[left]:
+                if target<nums[mid] or target>nums[right]:
                     right=mid-1
-                # elif target<=nums[left]:
                 else:
                     left=mid+1
-                # elif target>nums[right]:
-                    # right=mid-1
-    
-        if nums[left]==target:
-            return left
         return -1
-    
-print(Solution().search([4,5,6,7,0,1,2],4))                
