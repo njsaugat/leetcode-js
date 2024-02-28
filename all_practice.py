787e9b86e5611ff0ba82ceb0a0a0e19bc7a9f770
njsaugat <njsaugat@gmail.com>
2024-02-28 08:27:54 +0545

    daily prac

--- all_practice.py
+++ all_practice.py
@@ -349,5 +349,32 @@ class Solution:
         
         return charset
 
+    
+    
+    def search_rotated(self,nums,target):
         
+        left,right=0,len(nums)-1
+        
+        while left<=right:
+            mid=left+(right-left)//2
+            
+            if nums[mid]==target:
+                return mid
+            
+            # left sorted position
+            # 4,5,6,7,0,1,2
+            # 7,0,1,2,4,5,6,
+            elif nums[left]<=nums[mid]:
+
+                if target>nums[mid] or nums[left]>target:
+                    left=mid+1 
+                else:
+                    right=mid-1
+            else:
+                if target<nums[mid] or nums[right]<target:
+                    right=mid-1
+                else:
+                    left=mid+1
+        
+        return -1
 print(Solution().most_repeating("ABABBBA",1))
