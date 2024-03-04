1dd3616bff7ad49ca2e631f642a79405b25fe757
njsaugat <njsaugat@gmail.com>
2024-03-04 10:31:53 +0545

    monday

--- monday.py
+++ monday.py
@@ -539,4 +539,18 @@ class Solution:
             dp[i]=1+dp[i-offset]
             
         return offset
-                
+    
+    def validBST(self,root):
+        
+        
+        def isValid(node,leftVal,rightVal):
+            
+            if not node:
+                return True
+            
+            if not(leftVal<node.val<rightVal):
+                return False
+            
+            return isValid(node.left,leftVal ,node.val) and isValid(node.right,node.val,rightVal)
+        
+        return isValid(root,float("-inf"),float("inf"))
