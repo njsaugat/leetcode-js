2bbae8c18c11d25a686ad3db8a534d61b73bd895
njsaugat <njsaugat@gmail.com>
2024-03-04 08:50:31 +0545

    daily

--- dailyPrac.py
+++ dailyPrac.py
@@ -521,6 +521,21 @@ class Solution:
             output[repr(letters)].append(word)
         
         return [value for key,value in output.items()]
+
+
+    def productExceptSelf(self,nums):
+        output=[1]*len(nums)    
+
+        for i in range(len(nums)):
+            output[i]=output[i-1]*nums[i-1]
+        
+        postfix=1
+        for i in range(len(nums)-1,-1,-1):
+            # 12, 8, 6
+            output[i]=output[i]*postfix
+            postfix=postfix*nums[i]
             
+        return output
+
 
 print(Solution().groupAnagrams(["eat","tea","tan","ate","nat","bat"]))
