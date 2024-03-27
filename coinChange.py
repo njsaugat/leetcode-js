c809ed59dd0928a055565b42b456c4674b2e888e
njsaugat <njsaugat@gmail.com>
2024-03-27 13:30:12 +0545

    fibonacci

--- coinChange.py
+++ coinChange.py
@@ -99,3 +99,132 @@ class Solution:
         return dp[amount] if dp[amount]!=amount+1 else -1
     
     
+    
+    
+    
+    def coinChange(self,amount,coins):
+        
+        
+        dp=[amount+1] *(amount+1)
+
+        for amt in range(1,amount+1):
+            for coin in coins:
+                
+                if amt-coin>=0:
+                    dp[amt]=min(dp[amt],1+dp[amt-coin])
+
+        return dp[amount] if dp[amount]!=amount+1 else -1
+    
+
+    def coinChange(self,amount,coins):
+        
+        dp=[amount+1] *(amount+1)
+        dp[0]=0
+        
+        for amt in range(1,amount+1):
+            for coin in coins:
+                if amt-coin >=0:
+                    dp[amt]=min(dp[amt],1+dp[amt-coin])
+        
+        return dp[amount] if dp[amount]!=amount+1 else -1
+    
+    
+    def coinChange(self,amount,coins):
+        
+        dp=[amount+1]*(amount+1)
+
+        dp[0]=0
+        
+        for amt in range(1,amount+1):
+            for coin in coins:
+                if amt-coin >=0:
+                    dp[amt]=min(dp[amt],dp[amt-coin])
+        
+        return dp[amount] if dp[amount]!=amount+1 else -1
+    
+    
+    def coinChange(self,amount,coins):
+        
+        dp=[amount+1] *(amount+1)
+
+        dp[0]=0
+        
+        for amt in range(1,amount+1):
+            for coin in coins:
+                if amt>=coin:
+                    dp[amt]=min(dp[amt],1+dp[amt-coin])
+        return dp[amount] if dp[amount]!=amount+1 else -1
+    
+    
+    
+    def coinChange(self,amount,coins):
+        dp=[amount+1]*(amount+1)
+
+        dp[0]=0
+        
+        for amt in range(1,amount+1):
+            for coin in coins:
+                if amt-coin>=0:
+                    dp[amt]=min(dp[amt],1+dp[amt-coin])
+            
+            
+        return dp[amount] if dp[amount]!=amount+1 else -1
+    
+    
+    
+    
+    def coinChange(self,amount,coins):
+        dp=[amount+1]*(amount+1)
+        dp[0]=0
+        
+        for amt in range(1,amount+1):
+            for coin in coins:
+                if amt-coin>=0:
+                    dp[amt]=min(dp[amt],1+dp[amt-coin])
+        
+        
+        return dp[amount] if dp[amount]!=amount+1 else -1
+    
+    
+    
+    def coinChange(self,amount,coins):
+        dp=[amount+1]*(amount+1)
+        dp[0]=0
+        
+        for amt in range(1,amount+1):
+            for coin in coins:
+                if amt-coin>=0:
+                    dp[amt]=min(dp[amt],1+dp[amt-coin])
+        
+        
+        return dp[amount] if dp[amount]!=amount+1 else -1
+    
+    def coinChange(self,amount,coins):
+        dp=[amount+1]*(amount+1)
+
+        dp[0]=0
+        
+        for amt in range(1,amount+1):
+            for coin in coins:
+                
+                if amt-coin >=0:
+                    dp[amt]=min(dp[amt],1+dp[amt-coin])
+            
+        return dp[amount] if dp[amount]!=amount+1 else -1
+    
+    def coinChange(self,amount,coins):
+        dp=[amount+1]*(amount+1)
+
+        dp[0]=0
+        
+        for amt in range(1,amount+1):
+            for coin in coins:
+                
+                if amt-coin>=0:
+                    dp[amt]=min(dp[amt],1+dp[amt-coin])
+                
+        return dp[amount] if dp[amount]!=amount+1 else -1
+    
+    
+                    
+        
